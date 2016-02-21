0a5c6ce2abca3a2f8d282e49859b000795b97021
dpleshakov <dpleshakov@gmail.com>
2016-02-21 19:43:30 +0300

    Next version of commands

--- LJ.py
+++ LJ.py
@@ -18,7 +18,8 @@ CLEAR_RULES = [
 
 COUNT_RULES = [
     (r'</?a.*?>', ''),
-    (r'<img ', '<img style="border: 2px solid black;" width="1100" ')
+    (r'<img ', '<img style="border: 2px solid black;" width="1100" '),
+    (r'\n{2,}', '\n'),
 ]
 
 
@@ -28,32 +29,41 @@ def _make_rules(text, rules):
     return text
 
 
-def prepare(text):
-    return _make_rules(text, CLEAR_RULES)
-
-
-def count(text):
-    lines = text.split('\n')
-    for index in range(len(lines)):
-        lines[index] = _make_rules(lines[index], COUNT_RULES)
-        lines[index] = '\n' + str(index + 1) + ". \n" + lines[index]
-    return '\n'.join(lines)
-
-
-def main():
-	result = ""
-	with open('test.txt', 'r') as h_file:
-		result = prepare(h_file.read())
-
-	result = count(result)
-	print(result)
-
-
-class LjPrepareCommand(sublime_plugin.TextCommand):
+class LjPrepareTextCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         all_file_region = sublime.Region(0, self.view.size())
         text = self.view.substr(all_file_region)
-        text = prepare(text)
+        text = _make_rules(text, CLEAR_RULES)
+        self.view.replace(edit, all_file_region, text)
+
+
+class LjCountLinesCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for region in self.view.sel():
+            if not region.empty():
+                text = self.view.substr(region) + '\n'
+                self.view.replace(edit, region, '')
+                self.view.insert(edit, 0, text)
+        self.view.sel().clear()
+
+        all_file_region = sublime.Region(0, self.view.size())
+        text = self.view.substr(all_file_region)
+        text = _make_rules(text, COUNT_RULES)
+        lines = text.split('\n')
+        first_line = lines[0]
+        lines = lines[1:]
+        for index in range(len(lines)):
+            lines[index] = str(index + 1) + ". \n" + lines[index]
+            if index != len(lines):
+                lines[index] += '\n'
+        
+        lines.insert(0, '<lj-cut text="Читать дальше">')
+        lines.insert(0, '<a href=""><b>Оглавление</b></a>\n')
+        lines.insert(0, first_line + '\n')
+        lines.append('</lj-cut>')
+        lines.append('<lj-like buttons="repost,facebook,twitter,google,vkontakte,livejournal" />')
+        text = '\n'.join(lines)
+
         self.view.replace(edit, all_file_region, text)
 
 
